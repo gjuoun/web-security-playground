f5495be78ddba8ed425a98c3197ca5ef306c2285
Jun Guo <gjuoun@gmail.com>
2022-07-21 13:21:50 -0300

    error handler

--- csrf/node-ts/src/app.ts
+++ csrf/node-ts/src/app.ts
@@ -1,9 +1,25 @@
 import cookieParser from "cookie-parser";
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
 app.use(cookieParser("secret"));
+
+app.get("/error", (req, res) => {
+  throw new Error("Error occurred");
+  res.send("hey");
+});
+
+app.get("/error-async", (req, res, next) => {
+  fs.readFile(path.join(__dirname, "123.text"), (err, data) => {
+    if (err) {
+      next(err);
+    }
+    res.send(data);
+  });
+});
 
 app.get("/get-cookie", (req, res) => {
   res.cookie("X-Jun-Cookie", "12345", {
